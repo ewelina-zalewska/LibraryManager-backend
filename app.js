99e3f613896de0a99640cb86f834d64d6970deae
Ewelina <ewe.zale@gmail.com>
2025-02-11 09:59:52 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+﻿import express from "express";
+import bodyParser from "body-parser";
+
+import cors from "cors";
+import booksRoutes from "./routes/books.js";
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use("/books", booksRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Hello from Server");
+});
+
+app.listen(PORT, () => {
+  console.log(`server listening on http://localhost:${PORT}`);
+});
