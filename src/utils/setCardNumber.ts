5aeee0b4d3227ceb0cddf524dfe4af389ee5824e
Ewelina <ewe.zale@gmail.com>
2025-02-14 17:44:14 +0100

    set card number

---
+++ src/utils/setCardNumber.ts
@@ -0,0 +1,10 @@
+﻿export const setCardNumber = (name: string) => {
+  const randomId = Math.round(Math.random() * 10000);
+  const letters = name.slice(0, 2).toUpperCase();
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const cardNo = `${letters}${year}${month}${randomId}`;
+
+  return cardNo;
+};
