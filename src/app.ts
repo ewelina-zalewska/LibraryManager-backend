bb6709c166f4b299ece72fbf3ac3bcde577905cd
Ewelina <ewe.zale@gmail.com>
2025-02-14 14:00:45 +0100

    create app module

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+﻿import express from "express";
+import bodyParser from "body-parser";
+
+import cors from "cors";
+import booksRoutes from "#routes/books.ts";
+import registerRoutes from "#routes/register.ts";
+
+const app = express();
+app.use(express.json());
+const PORT = 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/books", booksRoutes);
+app.use("/register", registerRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Hello from Server");
+});
+
+app.listen(PORT, () => {
+  console.log(`server listening on http://localhost:${PORT}`);
+});
