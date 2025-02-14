201f650c5bf82cdf1495c9e92f127c9e9d1ab27a
Ewelina <ewe.zale@gmail.com>
2025-02-14 13:54:13 +0100

    create types

---
+++ src/types/index.ts
@@ -0,0 +1,27 @@
+﻿export interface Book {
+  id: string;
+  title: string;
+  author: string;
+  copies: number;
+  description: string;
+  releaseYear: string;
+}
+
+export interface User {
+  id: string;
+  username: string;
+  email: string;
+  password: string;
+  bookId: string[];
+}
+
+export interface UserRequest {
+  id: string;
+  username: string;
+  email: string;
+  password: string;
+}
+
+export interface UsersJSON {
+  users: User[];
+}
