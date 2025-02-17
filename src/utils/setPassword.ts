fe5889c987e4f42ae23282ebcd0e92682af11795
Ewelina <ewe.zale@gmail.com>
2025-02-17 16:52:15 +0100

    create a string

---
+++ src/utils/setPassword.ts
@@ -0,0 +1,7 @@
+﻿import { createHmac } from "crypto";
+
+export const setPassword = (originalText: string, number: string) => {
+  const salt = `${originalText}${number}mqwjkelrtoPASGDFHX0982364751<>?:"}{][';/.,+_)(*!@#$%^&PhdBXMC,EWADF\|VVnmbvcxzsrykjk)]}`;
+  const hash = createHmac("sha512", salt).update(originalText).digest("hex");
+  return hash.slice(0, 15);
+};
