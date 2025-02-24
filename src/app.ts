2171718de572ae0899b380ebc3ce40affa6bdddc
Ewelina <ewe.zale@gmail.com>
2025-02-24 12:08:21 +0100

    add new routes

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,43 @@
 ﻿import express from "express";
 import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
 
 import cors from "cors";
+import { getData } from "./config/getData.ts";
 import booksRoutes from "#routes/books.ts";
 import registerRoutes from "#routes/register.ts";
 import logsRoutes from "#routes/logs.ts";
+import loginRoutes from "#routes/login.ts";
+import logoutRoutes from "#routes/logout.ts";
+import authAdminRoutes from "#routes/auth/admin.ts";
+import authUserRoutes from "#routes/auth/user.ts";
+
+const { PORT, FRONTEND_PORT } = getData();
+
+const corsOptions = {
+  origin: `http://localhost:${FRONTEND_PORT}`,
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
 
 const app = express();
 app.use(express.json());
-const PORT = 3000;
 
-app.use(cors());
-app.use(bodyParser.json());
+app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cookieParser());
 
 app.use("/books", booksRoutes);
 app.use("/register", registerRoutes);
 app.use("/logs", logsRoutes);
+app.use("/auth/admin", authAdminRoutes);
+app.use("/auth/user", authUserRoutes);
+app.use("/login", loginRoutes);
+app.use("/logout", logoutRoutes);
 
 app.get("/", (req, res) => {
-  res.send("Hello from Server");
+  res.sendStatus(200);
 });
 
 app.listen(PORT, () => {
