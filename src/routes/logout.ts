54e36cc330e9c6e001f2e65929ed0113a73805cb
Ewelina <ewe.zale@gmail.com>
2025-02-24 12:11:47 +0100

    add logout module

---
+++ src/routes/logout.ts
@@ -0,0 +1,25 @@
+﻿import { withAuth } from "#utils/withAuth.ts";
+import express, { Request, Response } from "express";
+
+const router = express.Router();
+
+router.get("/", (req: Request, res: Response) => {
+  res.end();
+});
+
+router.post("/", withAuth, (req: Request, res: Response) => {
+  const { login } = req;
+  const { message } = req.body;
+
+  if (message !== "Logout") return;
+
+  res.clearCookie("token");
+  res.json({
+    status: "success",
+    message: "You have logged out successfully",
+    action: message,
+    id: login,
+  });
+});
+
+export default router;
